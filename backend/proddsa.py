a3621575dbe3a9ef58bfb2710c4f7f22c0dc6a13
Sanchay Naresh Gupta <sanchaynareshgupta@gmail.com>
2026-04-03 23:17:19 +0530

    Delete backend/proddsa.py

--- backend/proddsa.py
+++
@@ -1,6 +0,0 @@
-from random import sample
-
-import pandas as pd
-df = pd.read_csv("../data/processed/final_dataset.csv")
-print(df['store_id'].unique())
-sample_input = sample.drop(columns=["sales"])
